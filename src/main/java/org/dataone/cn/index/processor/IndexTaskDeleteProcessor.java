137b2765abab05f2d0f39a6a9e74a59fb940c768
Sky Roseboom <sroseboo@epscor.unm.edu>
2012-08-27 21:43:38 +0000

    redmine 3158.  Updating index delete logic to wait until resource map's object path is available before processing delete.  prevents erroneous error messages in log.

--- src/main/java/org/dataone/cn/index/processor/IndexTaskDeleteProcessor.java
+++ src/main/java/org/dataone/cn/index/processor/IndexTaskDeleteProcessor.java
@@ -31,7 +31,6 @@ import javax.xml.xpath.XPathExpressionException;
 import org.apache.commons.codec.EncoderException;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.dataone.cn.hazelcast.HazelcastClientInstance;
 import org.dataone.cn.index.task.IndexTask;
 import org.dataone.cn.index.task.IndexTaskRepository;
 import org.dataone.cn.indexer.XPathDocumentParser;
@@ -41,14 +40,9 @@ import org.dataone.cn.indexer.solrhttp.HTTPService;
 import org.dataone.cn.indexer.solrhttp.SolrDoc;
 import org.dataone.cn.indexer.solrhttp.SolrElementAdd;
 import org.dataone.cn.indexer.solrhttp.SolrElementField;
-import org.dataone.configuration.Settings;
-import org.dataone.service.types.v1.Identifier;
-import org.dataone.service.types.v1.SystemMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.HibernateOptimisticLockingFailureException;
 import org.w3c.dom.Document;
-
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
 
 public class IndexTaskDeleteProcessor implements IndexTaskProcessingStrategy {
 
@@ -63,22 +57,10 @@ public class IndexTaskDeleteProcessor implements IndexTaskProcessingStrategy {
     @Autowired
     private ArrayList<XPathDocumentParser> documentParsers;
 
-    private HazelcastInstance hzClient;
-
-    private static final String HZ_OBJECT_PATH = Settings.getConfiguration().getString(
-            "dataone.hazelcast.objectPath");
-
-    private static final String HZ_SYSTEM_METADATA = Settings.getConfiguration().getString(
-            "dataone.hazelcast.systemMetadata");
-
-    private IMap<Identifier, String> objectPaths;
-    private IMap<Identifier, SystemMetadata> systemMetadata;
-
     private String solrQueryUri;
     private String solrIndexUri;
 
     public void process(IndexTask task) throws Exception {
-        startHazelClient();
         if (isDataPackage(task)) {
             removeDataPackage(task);
         } else if (isPartOfDataPackage(task)) {
@@ -89,45 +71,58 @@ public class IndexTaskDeleteProcessor implements IndexTaskProcessingStrategy {
     }
 
     private void removeDataPackage(IndexTask task) throws Exception {
-        Document resourceMapDoc = getXPathDocumentParser().loadDocument(task.getObjectPath());
-        ResourceMap resourceMap = new ResourceMap(resourceMapDoc);
-        List<String> documentIds = resourceMap.getAllDocumentIDs();
-        List<SolrDoc> indexDocuments = httpService.getDocuments(solrQueryUri, documentIds);
-        removeFromIndex(task);
-        List<SolrDoc> docsToUpdate = new ArrayList<SolrDoc>();
-        // for each document in data package:
-        for (SolrDoc indexDoc : indexDocuments) {
+        if (task.getObjectPath() != null) {
+            Document resourceMapDoc = getXPathDocumentParser().loadDocument(task.getObjectPath());
+            if (resourceMapDoc != null) {
+                ResourceMap resourceMap = new ResourceMap(resourceMapDoc);
+                List<String> documentIds = resourceMap.getAllDocumentIDs();
+                List<SolrDoc> indexDocuments = httpService.getDocuments(solrQueryUri, documentIds);
+                removeFromIndex(task);
+                List<SolrDoc> docsToUpdate = new ArrayList<SolrDoc>();
+                // for each document in data package:
+                for (SolrDoc indexDoc : indexDocuments) {
 
-            if (indexDoc.getIdentifier().equals(task.getPid())) {
-                continue; // skipping the resource map, no need update it. will
-                          // be removed.
-            }
-
-            // Remove resourceMap reference
-            indexDoc.removeFieldsWithValue(SolrElementField.FIELD_RESOURCEMAP,
-                    resourceMap.getIdentifier());
-
-            // // Remove documents/documentedby values for this resource map
-            for (ResourceEntry entry : resourceMap.getMappedReferences()) {
-                if (indexDoc.getIdentifier().equals(entry.getIdentifier())) {
-                    for (String documentedBy : entry.getDocumentedBy()) {
-                        // Using removeOneFieldWithValue in-case same documents
-                        // are in more than one data package. just remove one
-                        // instance of data package info.
-                        indexDoc.removeOneFieldWithValue(SolrElementField.FIELD_ISDOCUMENTEDBY,
-                                documentedBy);
+                    if (indexDoc.getIdentifier().equals(task.getPid())) {
+                        continue; // skipping the resource map, no need update
+                                  // it.
+                                  // will
+                                  // be removed.
                     }
-                    for (String documents : entry.getDocuments()) {
-                        indexDoc.removeOneFieldWithValue(SolrElementField.FIELD_DOCUMENTS,
-                                documents);
+
+                    // Remove resourceMap reference
+                    indexDoc.removeFieldsWithValue(SolrElementField.FIELD_RESOURCEMAP,
+                            resourceMap.getIdentifier());
+
+                    // // Remove documents/documentedby values for this resource
+                    // map
+                    for (ResourceEntry entry : resourceMap.getMappedReferences()) {
+                        if (indexDoc.getIdentifier().equals(entry.getIdentifier())) {
+                            for (String documentedBy : entry.getDocumentedBy()) {
+                                // Using removeOneFieldWithValue in-case same
+                                // documents
+                                // are in more than one data package. just
+                                // remove
+                                // one
+                                // instance of data package info.
+                                indexDoc.removeOneFieldWithValue(
+                                        SolrElementField.FIELD_ISDOCUMENTEDBY, documentedBy);
+                            }
+                            for (String documents : entry.getDocuments()) {
+                                indexDoc.removeOneFieldWithValue(SolrElementField.FIELD_DOCUMENTS,
+                                        documents);
+                            }
+                            break;
+                        }
                     }
-                    break;
+                    docsToUpdate.add(indexDoc);
                 }
+                SolrElementAdd addCommand = new SolrElementAdd(docsToUpdate);
+                httpService.sendUpdate(solrIndexUri, addCommand);
             }
-            docsToUpdate.add(indexDoc);
+        } else if (task.getObjectPath() == null) {
+            task.markNew();
+            saveTask(task);
         }
-        SolrElementAdd addCommand = new SolrElementAdd(docsToUpdate);
-        httpService.sendUpdate(solrIndexUri, addCommand);
     }
 
     private void removeFromDataPackage(IndexTask task) throws Exception {
@@ -183,19 +178,21 @@ public class IndexTaskDeleteProcessor implements IndexTaskProcessingStrategy {
         return documentParsers.get(0);
     }
 
-    private void startHazelClient() {
-        if (this.hzClient == null) {
-            this.hzClient = HazelcastClientInstance.getHazelcastClient();
-            this.objectPaths = this.hzClient.getMap(HZ_OBJECT_PATH);
-            this.systemMetadata = hzClient.getMap(HZ_SYSTEM_METADATA);
-        }
-    }
-
     public void setSolrQueryUri(String uri) {
         this.solrQueryUri = uri;
     }
 
     public void setSolrIndexUri(String uri) {
         this.solrIndexUri = uri;
+    }
+
+    private IndexTask saveTask(IndexTask task) {
+        try {
+            task = repo.save(task);
+        } catch (HibernateOptimisticLockingFailureException e) {
+            logger.debug("Unable to update index task for pid: " + task.getPid() + ".");
+            task = null;
+        }
+        return task;
     }
 }
