2d6f0564d1d99bacfba89fd3e9c192dc6efcf144
Sky Roseboom <sroseboo@epscor.unm.edu>
2013-10-14 22:58:51 +0000

    updated to use d1 DateTimeMarshaller to convert xsd datetime strings to Date objects.

--- src/main/java/org/dataone/cn/indexer/convert/SolrDateConverter.java
+++ src/main/java/org/dataone/cn/indexer/convert/SolrDateConverter.java
@@ -24,34 +24,21 @@ package org.dataone.cn.indexer.convert;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
-/**
-  * User: Porter
+import org.dataone.service.util.DateTimeMarshaller;
+
+/**Converts date to solr consumable format.
+ * User: Porter
  * Date: 7/26/11
  * Time: 6:04 PM
  */
 
-/**Converts date to solr consumable format.
- *
- */
-
 public class SolrDateConverter implements IConverter{
-
-    /**Converts String data to solr consumable date format
-     *
-     */
-
 
     private static TimeZone OUTPUT_TIMEZONE = TimeZone.getTimeZone("Zulu");
 
-//    SOLR FORMAT 1995-12-31T23:59:59Z
-//    NOT 2005-07-21T14:55:30-0600
-    /**solr date format
-     *
-     * */
     protected static final String OUTPUT_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
     /**Converts date to solr consumable format.
@@ -66,10 +53,10 @@ public class SolrDateConverter implements IConverter{
         if(data == null || data.equals("")){
             return "";
         }
-        Calendar cal = javax.xml.bind.DatatypeConverter.parseDateTime(data);
+        Date dateTime = DateTimeMarshaller.deserializeDateToUTC(data);
         SimpleDateFormat sdf = new SimpleDateFormat(OUTPUT_DATE_FORMAT);
         sdf.setTimeZone(OUTPUT_TIMEZONE);
-        String outputDateFormat = sdf.format(cal.getTime());
+        String outputDateFormat = sdf.format(dateTime);
 
         return outputDateFormat;
     }
