8c2a70457a26694f5d451de66b9c73da7edc0836
Sky Roseboom <sroseboo@epscor.unm.edu>
2014-07-17 14:53:33 +0000

    removed http client dependency.  updated class path to ObjectFormatCache (compilation error).

--- src/main/java/org/dataone/cn/index/processor/IndexTaskProcessor.java
+++ src/main/java/org/dataone/cn/index/processor/IndexTaskProcessor.java
@@ -27,7 +27,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.dataone.client.formats.ObjectFormatCache;
+import org.dataone.client.ObjectFormatCache;
 import org.dataone.cn.hazelcast.HazelcastClientFactory;
 import org.dataone.cn.index.task.IndexTask;
 import org.dataone.cn.index.task.IndexTaskRepository;
