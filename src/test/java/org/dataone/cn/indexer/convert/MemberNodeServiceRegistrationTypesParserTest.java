23224614e402e3e52f4b8d3e2e28c5db8a11102f
Andrei Buium <andreib@dataone.unm.edu>
2016-01-14 22:13:23 +0000

    Updated service types parser test to reflect currentl match patterns

--- src/test/java/org/dataone/cn/indexer/convert/MemberNodeServiceRegistrationTypesParserTest.java
+++ src/test/java/org/dataone/cn/indexer/convert/MemberNodeServiceRegistrationTypesParserTest.java
@@ -58,12 +58,12 @@ public class MemberNodeServiceRegistrationTypesParserTest extends TestCase {
         assertTrue("Service types document should contain an OPeNDAP service", opendapType.getName().equals("OPeNDAP"));
         assertTrue("Service types document should contain a WMS service", wmsType.getName().equals("WMS"));
         
-        String OPENDAP_PATTERN_1 = "([Oo][Pp][Ee][Nn][Dd][Aa][Pp])";
+        String OPENDAP_PATTERN_1 = "(.*[Oo][Pp][Ee][Nn][Dd][Aa][Pp].*)";
         Collection<String> opendapPatterns = opendapType.getMatchingPatterns();
         assertTrue("OPeNDAP service type should contain match pattern: " + OPENDAP_PATTERN_1,
                 opendapPatterns.contains(OPENDAP_PATTERN_1));
         
-        String WMS_PATTERN_1 = "([Ww][Mm][Ss])";
+        String WMS_PATTERN_1 = "(.*[Ww][Mm][Ss].*)";
         Collection<String> wmsPatterns = wmsType.getMatchingPatterns();
         assertTrue("WMS service type should contain match pattern: " + WMS_PATTERN_1, 
                 wmsPatterns.contains(WMS_PATTERN_1));
