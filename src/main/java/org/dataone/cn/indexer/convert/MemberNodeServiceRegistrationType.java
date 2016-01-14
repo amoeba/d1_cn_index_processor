7567100a85ca42397e9af800b5045a5036d05317
Sky Roseboom <sroseboo@epscor.unm.edu>
2016-01-14 17:05:56 +0000

    pojo to represent controlled list of member node service registration service types

---
+++ src/main/java/org/dataone/cn/indexer/convert/MemberNodeServiceRegistrationType.java
@@ -0,0 +1,26 @@
+package org.dataone.cn.indexer.convert;
+
+import java.util.Collection;
+
+public class MemberNodeServiceRegistrationType {
+
+    private String name;
+    private Collection<String> matchingPatterns;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Collection<String> getMatchingPatterns() {
+        return matchingPatterns;
+    }
+
+    public void setMatchingPatterns(Collection<String> matchingPatterns) {
+        this.matchingPatterns = matchingPatterns;
+    }
+
+}
