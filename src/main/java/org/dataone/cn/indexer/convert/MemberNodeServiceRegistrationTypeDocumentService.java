75841b2a9050858661f088b0e633336f0e663e69
Andrei Buium <andreib@dataone.unm.edu>
2016-01-14 22:42:19 +0000

    Moving index processor changes related to member node service registration and service type conversion into v2.0 branch and relevant xml files into dataone-cn-index trunk and v2.0 branch.

---
+++ src/main/java/org/dataone/cn/indexer/convert/MemberNodeServiceRegistrationTypeDocumentService.java
@@ -0,0 +1,94 @@
+/**
+ * This work was created by participants in the DataONE project, and is
+ * jointly copyrighted by participating institutions in DataONE. For 
+ * more information on DataONE, see our web site at http://dataone.org.
+ *
+ *   Copyright ${year}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ * 
+ * $Id$
+ */
+package org.dataone.cn.indexer.convert;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.log4j.Logger;
+import org.dataone.cn.indexer.XmlDocumentUtility;
+import org.dataone.configuration.Settings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class MemberNodeServiceRegistrationTypeDocumentService {
+
+    private static Logger log = Logger
+            .getLogger(MemberNodeServiceRegistrationTypeDocumentService.class.getName());
+
+    private static final String SERVICE_DOC_LOCATION_URL = Settings.getConfiguration().getString(
+            "dataone.mn.registration.serviceType.url");
+
+    @Autowired
+    private HttpComponentsClientHttpRequestFactory httpRequestFactory;
+
+    public MemberNodeServiceRegistrationTypeDocumentService() {
+    }
+
+    public Document getMemberNodeServiceRegistrationTypeDocument() {
+        Document doc = null;
+        InputStream stream = fetchServiceTypeDoc();
+        if (stream != null) {
+            try {
+                doc = XmlDocumentUtility.generateXmlDocument(stream);
+            } catch (SAXException e) {
+                log.error("Unable to create w3c Document from input stream", e);
+                e.printStackTrace();
+            }
+        }
+        return doc;
+    }
+
+    private InputStream fetchServiceTypeDoc() {
+        InputStream stream = null;
+        HttpClient httpClient = httpRequestFactory.getHttpClient();
+        HttpGet get = new HttpGet(SERVICE_DOC_LOCATION_URL);
+        HttpResponse response;
+        try {
+            response = httpClient.execute(get);
+            HttpEntity entity = response.getEntity();
+            stream = entity.getContent();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+            log.error("Unable to fetch service type doc from: " + SERVICE_DOC_LOCATION_URL, e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error("Unable to fetch service type doc from: " + SERVICE_DOC_LOCATION_URL, e);
+        }
+        return stream;
+    }
+
+    protected String getServiceTypeDocUrl() {
+        return SERVICE_DOC_LOCATION_URL;
+    }
+
+    protected HttpComponentsClientHttpRequestFactory getHttpClientFactory() {
+        return httpRequestFactory;
+    }
+}
---
+++ src/test/java/org/dataone/cn/indexer/convert/MemberNodeServiceRegistrationTypeConverterTest.java
@@ -0,0 +1,81 @@
+/**
+ * This work was created by participants in the DataONE project, and is
+ * jointly copyrighted by participating institutions in DataONE. For 
+ * more information on DataONE, see our web site at http://dataone.org.
+ *
+ *   Copyright ${year}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ * 
+ * $Id$
+ */
+package org.dataone.cn.indexer.convert;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "../../index/test-context.xml" })
+public class MemberNodeServiceRegistrationTypeConverterTest {
+
+    @Autowired
+    private MemberNodeServiceRegistrationTypeConverter serviceTypesConverter;
+
+    private static final String WMS_SERVICE_NAME = "WMS";
+    private static final String OPOENDAP_SERVICE_NAME = "OPeNDAP";
+    
+    @Test
+    public void testConvertWMS() {
+        assertTrue("\"WMS\" should be converted to service type " + WMS_SERVICE_NAME + ".",
+                WMS_SERVICE_NAME.equals(serviceTypesConverter.convert("WMS")));
+        assertTrue("\"Wms\" should be converted to service type " + WMS_SERVICE_NAME + ".",
+                WMS_SERVICE_NAME.equals(serviceTypesConverter.convert("Wms")));
+        assertTrue("\"wms\" should be converted to service type " + WMS_SERVICE_NAME + ".",
+                WMS_SERVICE_NAME.equals(serviceTypesConverter.convert("wms")));
+        
+        assertTrue("\"==WMS==\" should be converted to service type " + WMS_SERVICE_NAME + ".",
+                WMS_SERVICE_NAME.equals(serviceTypesConverter.convert("==WMS==")));
+        assertTrue("\"Open Geospatial Consortium Web Map Service (WMS)\" should be converted to service type " + WMS_SERVICE_NAME + ".",
+                WMS_SERVICE_NAME.equals(serviceTypesConverter.convert("Open Geospatial Consortium Web Map Service (WMS)")));
+        
+        assertFalse("\"W-M-S\" should not be converted to service type " + WMS_SERVICE_NAME + ".",
+                WMS_SERVICE_NAME.equals(serviceTypesConverter.convert("W-M-S")));
+        assertFalse("\"OPeNDAP\" should not be converted to service type " + WMS_SERVICE_NAME + ".",
+                WMS_SERVICE_NAME.equals(serviceTypesConverter.convert("OPeNDAP")));
+    }
+
+    @Test
+    public void testConvertOPeNDAP() {
+        assertTrue("\"OPeNDAP\" should be converted to service type " + OPOENDAP_SERVICE_NAME + ".",
+                OPOENDAP_SERVICE_NAME.equals(serviceTypesConverter.convert("OPeNDAP")));
+        assertTrue("\"OPENDAP\" should be converted to service type " + OPOENDAP_SERVICE_NAME + ".",
+                OPOENDAP_SERVICE_NAME.equals(serviceTypesConverter.convert("OPENDAP")));
+        assertTrue("\"opendap\" should be converted to service type " + OPOENDAP_SERVICE_NAME + ".",
+                OPOENDAP_SERVICE_NAME.equals(serviceTypesConverter.convert("opendap")));
+
+        assertTrue("\"ERDDAP OPeNDAP\" should be converted to service type " + OPOENDAP_SERVICE_NAME + ".",
+                OPOENDAP_SERVICE_NAME.equals(serviceTypesConverter.convert("ERDDAP OPeNDAP")));
+
+        assertFalse("\"WMS\" should not be converted to service type " + OPOENDAP_SERVICE_NAME + ".",
+                OPOENDAP_SERVICE_NAME.equals(serviceTypesConverter.convert("WMS")));
+        assertFalse("\"ERRDAP\" should not be converted to service type " + OPOENDAP_SERVICE_NAME + ".",
+                OPOENDAP_SERVICE_NAME.equals(serviceTypesConverter.convert("ERRDAP")));
+    }
+
+}
