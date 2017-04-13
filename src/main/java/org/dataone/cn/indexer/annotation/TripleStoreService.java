b08dca07da5c31df53862153b224a34694e4e429
Jing Tao <tao@nceas.ucsb.edu>
2017-04-13 18:00:03 +0000

    Add a debug statement to identify the triple store location.

--- src/main/java/org/dataone/cn/indexer/annotation/TripleStoreService.java
+++ src/main/java/org/dataone/cn/indexer/annotation/TripleStoreService.java
@@ -2,13 +2,14 @@ package org.dataone.cn.indexer.annotation;
 
 import java.io.File;
 
+import org.apache.log4j.Logger;
 import org.dataone.configuration.Settings;
 
 import com.hp.hpl.jena.query.Dataset;
 import com.hp.hpl.jena.tdb.TDBFactory;
 
 public class TripleStoreService {
-
+    private static Logger log = Logger.getLogger(TripleStoreService.class);
 	private static TripleStoreService instance;
 	
 	private TripleStoreService() {}
@@ -22,7 +23,7 @@ public class TripleStoreService {
 	
 	public Dataset getDataset() {
 		String directory = Settings.getConfiguration().getString("index.tdb.directory", "./tdb");
-
+		log.info("TripleStoreService.getDataset - the triple store location ================= is "+directory);
     	// for testing, delete the triplestore each time
     	File dir = new File(directory);
     	if (!dir.exists()) {
