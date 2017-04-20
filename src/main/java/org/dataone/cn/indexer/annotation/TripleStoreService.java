280e27f67d28fa0b9ccddc12bf77ae45b5cafb75
Jing Tao <tao@nceas.ucsb.edu>
2017-04-20 23:08:10 +0000

    Create a subdirectory as a tdb store for each dataset.

--- src/main/java/org/dataone/cn/indexer/annotation/TripleStoreService.java
+++ src/main/java/org/dataone/cn/indexer/annotation/TripleStoreService.java
@@ -1,7 +1,13 @@
 package org.dataone.cn.indexer.annotation;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+import java.util.Hashtable;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.dataone.configuration.Settings;
 
@@ -11,6 +17,7 @@ import com.hp.hpl.jena.tdb.TDBFactory;
 public class TripleStoreService {
     private static Logger log = Logger.getLogger(TripleStoreService.class);
 	private static TripleStoreService instance;
+	private static Hashtable<Dataset, File> dataset_location_map = new Hashtable<Dataset, File>();
 	
 	private TripleStoreService() {}
 	
@@ -21,19 +28,35 @@ public class TripleStoreService {
 		return instance;
 	}
 	
-	public Dataset getDataset() {
+	public Dataset getDataset() throws IOException {
 		String directory = Settings.getConfiguration().getString("index.tdb.directory", "./tdb");
-		log.info("TripleStoreService.getDataset - the triple store location ================= is "+directory);
+		log.info("TripleStoreService.getDataset - the parent directory of the triple store location ================= is "+directory);
     	// for testing, delete the triplestore each time
     	File dir = new File(directory);
     	if (!dir.exists()) {
         	dir.mkdirs();
-
     	}
-//    	if (dir.exists()) {
-//    		dir.delete();
-//    	}
-		Dataset dataset = TDBFactory.createDataset(directory);
+    	String prefix ="tdb";
+    	Path store = Files.createTempDirectory(dir.toPath(), prefix, new FileAttribute<?>[] {});
+    	FileUtils.forceDeleteOnExit(store.toFile());
+    	log.info("TripleStoreService.getDataset - the store directory  ================= is "+store.toString());
+		Dataset dataset = TDBFactory.createDataset(store.toString());
+		dataset_location_map.put(dataset, store.toFile());
 		return dataset;
+	}
+	
+	/**
+	 * Destroy the given dataset (delete the backup store)
+	 * @param dataset
+	 */
+	public void destoryDataset(Dataset dataset) throws IOException {
+	    if(dataset != null) {
+	        File file = dataset_location_map.remove(dataset);
+	        if(file != null) {
+	            FileUtils.deleteDirectory(file);	           
+	            log.debug("The direcotry was deleted "+file.getAbsolutePath());
+	        }
+	        log.debug("The size of hashmap is "+dataset_location_map.size());
+	    }
 	}
 }
