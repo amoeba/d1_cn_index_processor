d8f0111185e8e71e62e0df33177d9b1554756012
Rob Nahf <rnahf@epscor.unm.edu>
2017-06-29 21:03:16 +0000

    removing unused imports holding up compilation.

--- src/main/java/org/dataone/cn/index/messaging/MessagingClientConfiguration.java
+++ src/main/java/org/dataone/cn/index/messaging/MessagingClientConfiguration.java
@@ -1,14 +1,11 @@
 package org.dataone.cn.index.messaging;
 
-import org.apache.http.protocol.HttpService;
 import org.dataone.cn.index.processor.IndexTaskTriageMessageProcessor;
 import org.dataone.cn.index.processor.IndexingMessageProcessor;
 import org.dataone.cn.index.processor.ResourceMapReadinessMessageProcessor;
 import org.dataone.cn.indexer.D1IndexerSolrClient;
 import org.dataone.cn.indexer.SolrIndexService;
-import org.dataone.cn.indexer.processor.MockD1IndexerSolrClient;
 import org.dataone.cn.messaging.QueueAccess;
-import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
@@ -16,10 +13,7 @@ import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.Profile;
 
 //@Profile("production")
 //@Configuration
